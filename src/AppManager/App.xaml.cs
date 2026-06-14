db078bf9f94005f5ccd5d1cd0b6eea336cc29e2b
Developer <developer@appmanager.local>
2026-06-14 13:52:54 +0800

    feat: add system tray support with minimize to tray

--- src/AppManager/App.xaml.cs
+++ src/AppManager/App.xaml.cs
@@ -4,4 +4,52 @@ namespace AppManager;
 
 public partial class App : System.Windows.Application
 {
+    private System.Windows.Forms.NotifyIcon? _trayIcon;
+
+    private void Application_Startup(object sender, StartupEventArgs e)
+    {
+        var mainWindow = new MainWindow();
+        mainWindow.Show();
+        CreateTrayIcon();
+    }
+
+    private void CreateTrayIcon()
+    {
+        _trayIcon = new System.Windows.Forms.NotifyIcon
+        {
+            Icon = System.Drawing.SystemIcons.Application,
+            Visible = true,
+            Text = "程序管理器"
+        };
+
+        var contextMenu = new System.Windows.Forms.ContextMenuStrip();
+        contextMenu.Items.Add("显示主窗口", null, (s, e) => ShowMainWindow());
+        contextMenu.Items.Add(new System.Windows.Forms.ToolStripSeparator());
+        contextMenu.Items.Add("退出", null, (s, e) => ExitApplication());
+
+        _trayIcon.ContextMenuStrip = contextMenu;
+        _trayIcon.DoubleClick += (s, e) => ShowMainWindow();
+    }
+
+    private void ShowMainWindow()
+    {
+        if (MainWindow != null)
+        {
+            MainWindow.Show();
+            MainWindow.WindowState = WindowState.Normal;
+            MainWindow.Activate();
+        }
+    }
+
+    private void ExitApplication()
+    {
+        _trayIcon?.Dispose();
+        Current.Shutdown();
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        _trayIcon?.Dispose();
+        base.OnExit(e);
+    }
 }
