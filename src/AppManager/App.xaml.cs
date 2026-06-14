3efa5fe48951042e4a373cfe76da530978c3e2f9
Developer <developer@appmanager.local>
2026-06-14 13:38:08 +0800

    chore: clean up template boilerplate usings

--- src/AppManager/App.xaml.cs
+++ src/AppManager/App.xaml.cs
@@ -1,13 +1,7 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
 
 namespace AppManager;
 
-/// <summary>
-/// Interaction logic for App.xaml
-/// </summary>
 public partial class App : System.Windows.Application
 {
 }
-
