6cfa984480e19f38108105c0185fecacb8d0cb3e
Developer <developer@appmanager.local>
2026-06-14 13:52:22 +0800

    feat: build main window UI with split layout and data binding

---
+++ src/AppManager/Converters/NullToVisibilityConverters.cs
@@ -0,0 +1,31 @@
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace AppManager.Converters;
+
+public class NullToCollapsedConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value == null ? Visibility.Collapsed : Visibility.Visible;
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class NullToVisibleConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value == null ? Visibility.Visible : Visibility.Collapsed;
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
--- src/AppManager/MainWindow.xaml.cs
+++ src/AppManager/MainWindow.xaml.cs
@@ -1,11 +1,23 @@
-﻿using System.Windows;
+﻿using System.ComponentModel;
+using System.Windows;
+using AppManager.ViewModels;
 
 namespace AppManager;
 
 public partial class MainWindow : Window
 {
+    private readonly MainViewModel _viewModel;
+
     public MainWindow()
     {
         InitializeComponent();
+        _viewModel = new MainViewModel();
+        DataContext = _viewModel;
+    }
+
+    private void Window_Closing(object sender, CancelEventArgs e)
+    {
+        e.Cancel = true;
+        Hide();
     }
 }
